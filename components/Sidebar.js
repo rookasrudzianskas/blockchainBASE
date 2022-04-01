0712e3ddcb5213f416dd983896056c60167f52f5
Rokas <rookas.rudzenskas@gmail.com>
2022-04-01 18:06:22 +0300

    Rokas

---
+++ components/Sidebar.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Sidebar = () => {
+    return (
+        <Wrapper>
+
+        </Wrapper>
+    );
+};
+
+export default Sidebar;
+
+const Wrapper = styled.div`
+  height: calc(100vh);
+  border-right: 1px solid #282b2f;
+  width: calc(22rem - 16px - 16px);
+  padding: 0 1rem;
+`;
+
+const LogoContainer = styled.div`
+  margin: 1.5rem 0;
+`;
+const Logo = styled.div`
+  width: 44%;
+  object-fit: contain;
+  margin-left: 1.5rem;
+`;
+
+const NavItemsContainer = styled.div`
+  margin-top: 3rem;
+  
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const NavItem = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 1.3rem;
+  font-weight: 500;
+  border-radius: 0.5rem;
+  margin-bottom: 1.5rem;
+  height: 4rem;
+  
+  &:hover {
+    background-color: #141519;
+  }
+`
+
+
