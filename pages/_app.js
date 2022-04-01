a29925e4f64fc0e77faf86fb78b691901224076f
Rokas <rookas.rudzenskas@gmail.com>
2022-04-01 09:48:01 +0300

    Rokas

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,19 @@
 import '../styles/globals.css';
-import {ThirdWeb3Provider } from '@3rdweb/hooks';
+import {ThirdwebProvider} from "@3rdweb/react";
+
+const supportedChainIds = [4];
+const connectors = {
+  injected: {},
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <ThirdwebProvider supportedChainIds={supportedChainIds} connectors={connectors}>
+        <Component {...pageProps} />
+      </ThirdwebProvider>
+      )
+
+
 }
 
 export default MyApp
