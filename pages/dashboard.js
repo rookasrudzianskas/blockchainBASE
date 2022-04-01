189265393d90dc8e5a52a92e282580d66fe9fe6a
Rokas <rookas.rudzenskas@gmail.com>
2022-04-01 15:47:59 +0300

    Rokas

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Header from "../components/Header";
+import styled from 'styled-components';
 
 const Dashboard = ({address}) => {
     return (
-        <>
+        <Wrapper>
             <Header />
-        </>
+        </Wrapper>
     );
 };
 
 export default Dashboard;
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vh;
+  background-color: #0a0b0d;
+  color: white;
+`
+const MainContainer = styled.div`
+  flex: 1;
+`
