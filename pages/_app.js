5b5d75065ce2e3a3494d5672833d37202f1d7a6c
Rokas <rookas.rudzenskas@gmail.com>
2022-04-01 09:45:06 +0300

    Rokas

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import {ThirdWeb3Provider } from '@3rdweb/hooks';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
