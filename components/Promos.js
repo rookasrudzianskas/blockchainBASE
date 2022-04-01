0bc7a3150790676d346d90ef9017d4bdc16b0e3b
Rokas <rookas.rudzenskas@gmail.com>
2022-04-01 21:06:59 +0300

    Rokas

---
+++ components/Promos.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Promos = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Promos;
+
+const Wrapper = styled.div`
+  margin-top: 2rem;
+  padding-right: 1rem;
+`;
+
+const OfferCard = styled.div`
+  width: 21rem;
+  height: 11rem;
+  border: 1px solid #282b2f;
+  margin-bottom: 1rem;
+  padding: 1.5rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.div`
+  font-weight: 700;
+  font-size: 1.5rem;
+  margin-bottom: 0.1rem;
+`;
+
+const Description = styled.div`
+  font-size: 1.1rem;
+`
