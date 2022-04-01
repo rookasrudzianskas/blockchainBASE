25f12a949e1e3d74e628e9a93968ed517eac6ae6
Rokas <rookas.rudzenskas@gmail.com>
2022-04-01 15:56:35 +0300

    Rokas

---
+++ components/Portfolio.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <Wrapper>
+
+        </Wrapper>
+    );
+};
+
+export default Portfolio;
+
+const Wrapper = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  max-width: 100px;
+  padding: 2rem 1rem;
+`
+
+// const PortfolioTable = styled.div`
+//   margin-top: 1rem;
+//   border: 1px solid #282b2f;
+// `;
+
+const Table = styled.div`
+  width: 100%;
+`
