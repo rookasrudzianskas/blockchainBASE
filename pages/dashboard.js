d83363a6039d26d84329827524eee5ce05eeaecc
Rokas <rookas.rudzenskas@gmail.com>
2022-04-02 17:47:32 +0300

    Rokas

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from "../components/Header";
 import styled from 'styled-components';
 import Main from "../components/Main";
 import Sidebar from "../components/Sidebar";
+import {ethers} from 'ethers';
+import  {ThirdwebSDK} from "@3rdweb/sdk";
+
+const sdk = new ThirdwebSDK(
+    new ethers.Wallet(
+        process.env.NEXT_PUBLIC_METAMASK_KEY,
+        ethers.getDefaultProvider(
+            'https://rinkeby.infura.io/v3/'
+        )
+    )
+);
 
 const Dashboard = ({address}) => {
+    const [sanityTokens, setSanityTokens] = useState([]);
+    const [thirdWebTokens, setThirdWebTokens] = useState([]);
+
+    useEffect(() => {
+        const getSanityAndThirdWebTokens = async () => {
+            const coins = await fetch(
+                "https://t9otsq1j.api.sanity.io/v1/data/query/production?query=*%5B_type%20%3D%3D%20%27coins%27%5D%20%7B%0A%20%20name%2C%0A%20%20symbol%2C%0A%20%20contractAddress%2C%0A%20%20logo%2C%0A%20%20usdPrice%0A%7D",
+            )
+            const sanityTokens = (await coins.json()).result
+            setSanityTokens(sanityTokens);
+
+            setThirdWebTokens(sanityTokens.map(token => sdk.getTokenModule(token.contractAddress)));
+        }
+
+        getSanityAndThirdWebTokens().then()
+    }, []);
+
+    console.log('Sanity 👉', sanityTokens);
+    console.log('ThirdWeb 👉', thirdWebTokens);
+
+
     return (
         <Wrapper>
             <Sidebar />
