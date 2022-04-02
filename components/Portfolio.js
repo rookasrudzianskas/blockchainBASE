a38b7481b061f8f390a635663e36e6156dccba3e
Rokas <rookas.rudzenskas@gmail.com>
2022-04-02 09:38:44 +0300

    Rokas

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -4,14 +4,44 @@ import {BsThreeDotsVertical} from "react-icons/bs";
 import {coins} from "../static/coins";
 import Coin from "./Coin";
 import BalanceChart from "./BalanceChart";
+import NextCors from 'nextjs-cors';
 
 const Portfolio = () => {
     const [sanityTokens, setSanityTokens] = useState([]);
 
+    // useEffect(() => {
+    //     async function handler(req, res) {
+    //         // Run the cors middleware
+    //         // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+    //         await NextCors(req, res, {
+    //             // Options
+    //             methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    //             origin: '*',
+    //             optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    //         });
+    //
+    //         // Rest of the API logic
+    //         res.json({ message: 'Hello NextJs Cors!' });
+    //     }
+    //
+    //     handler().then();
+    // }, [])
+
     useEffect(() => {
         const getCoins = async () => {
             try {
-                const coins = await fetch("https://t9otsq1j.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D'coins'%5D%20%7B%0A%20%20name%2C%0A%20%20usdPrice%2C%0A%20%20contractAddress%2C%0A%20%20symbol%2C%0A%20%20logo%0A%7D");
+                let headers = new Headers();
+                headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+                headers.append("Access-Control-Allow-Headers",
+                    "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+                headers.append("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+
+                const coins = await fetch("https://t9otsq1j.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D'coins'%5D%20%7B%0A%20%20name%2C%0A%20%20usdPrice%2C%0A%20%20contractAddress%2C%0A%20%20symbol%2C%0A%20%20logo%0A%7D", {
+                        mode: 'cors',
+                        credentials: 'include',
+                        method: 'POST',
+                        headers: headers
+                    });
                 const tempSanityTokens = await coins.json();
                 setSanityTokens(tempSanityTokens.result);
                 console.log("This is it", tempSanityTokens.result);
