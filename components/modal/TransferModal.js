e78ce4459f02634e3e74f1a34d67835a33982216
Rokas <rookas.rudzenskas@gmail.com>
2022-04-03 10:40:17 +0300

    Rokas

--- components/modal/TransferModal.js
+++ components/modal/TransferModal.js
@@ -3,11 +3,49 @@ import styled from 'styled-components';
 
 const TransferModal = () => {
     return (
-        <div>
-            <p>Send</p>
-            <p>receive</p>
-        </div>
+        <Wrapper>
+            <Selector>
+                <Option>
+                    <p>Send</p>
+                </Option>
+                <Option>
+                    <p>Receive</p>
+                </Option>
+            </Selector>
+        </Wrapper>
     );
 };
 
 export default TransferModal;
+
+const Wrapper = styled.div`
+  height: 35rem;
+  width: 27rem;
+  color: white;
+  border: 1px solid #282b2f;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Selector = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 5rem;
+`;
+
+const Option = styled.div`
+  height: 100%;
+  width: 100%;
+  display: grid;
+  place-items: center;
+  font-size: 1.2rem;
+  font-weight: 600;
+  
+  &:hover {
+    cursor: pointer;
+    background-color: #111214;
+  }
+`
+
+
