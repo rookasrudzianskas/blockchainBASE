cd05a0e48571a420c456916b604ba045458fbffb
Rokas <rookas.rudzenskas@gmail.com>
2022-04-04 09:05:18 +0300

    Rokas

---
+++ components/modal/CoinSelector.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CoinSelector = ({
+                          setAction,
+                          selectedToken,
+                          setSelectedToken,
+                          sanityTokens,
+                          twTokens,
+                          walletAddress,
+                      }) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default CoinSelector;
+
+const Wrapper = styled.div``
+const Title = styled.div`
+  width: 100%;
+  text-align: center;
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
+`
+const CoinList = styled.div`
+  display: flex;
+  flex-direction: column;
+`
