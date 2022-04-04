9cc537c9a221a962fa600520a276d70b4b59843c
Rokas <rookas.rudzenskas@gmail.com>
2022-04-04 09:14:10 +0300

    Rokas

--- components/modal/CoinItem.js
+++ components/modal/CoinItem.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const CoinItem = () => {
+const CoinItem = ({
+                      token,
+                      sender,
+                      setAction,
+                      selectedToken,
+                      setSelectedToken,
+                      sanityTokens,
+                      thirdWebTokens,
+                  }) => {
     return (
         <div>
-
+            {sanityTokens[0].name}
         </div>
     );
 };
